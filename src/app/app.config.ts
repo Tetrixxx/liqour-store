3a6f87eaafb0fbcc614939640d0894ac6e4832e7
Tetrixxx <danikokos@gmail.com>
2025-05-17 12:12:46 +0200

    firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,28 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideNativeDateAdapter } from '@angular/material/core';
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideNativeDateAdapter(),
+    provideFirebaseApp(() =>
+    initializeApp({
+      apiKey: "AIzaSyCd0qTHcmjOlCqi3W8ScYiGU7OmZn5OJSY",
+      authDomain: "liqour-store-69b89.firebaseapp.com",
+      projectId: "liqour-store-69b89",
+      storageBucket: "liqour-store-69b89.firebasestorage.app",
+      messagingSenderId: "860259198324",
+      appId: "1:860259198324:web:e8e5b06faa7c3e7c3e7821"
+    })),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+
   ]
 
 };
--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,24 @@ import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(),
+     provideFirebaseApp(() => 
+      initializeApp({ 
+        projectId: "liqour-store-69b89",
+        appId: "1:860259198324:web:e8e5b06faa7c3e7c3e7821", 
+        storageBucket: "liqour-store-69b89.firebasestorage.app", 
+        apiKey: "AIzaSyCd0qTHcmjOlCqi3W8ScYiGU7OmZn5OJSY", 
+        authDomain: "liqour-store-69b89.firebaseapp.com", 
+        messagingSenderId: "860259198324" })), 
+        provideAuth(() => 
+        getAuth()), 
+        provideFirestore(() => getFirestore())
   ]
 }).catch(err => console.error(err));
